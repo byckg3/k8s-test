2927bcde2ee4eec4fa43223b7dc4c27b98a36eb8
byc <byckg3@gmail.com>
2024-11-11 22:40:13 +0800

    add output for service

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 resource "kubernetes_manifest" "demo_deployment" {
-    manifest = provider::kubernetes::manifest_decode( file( "currency-demo-deployment.yaml" ) )
+    manifest = provider::kubernetes::manifest_decode( file( "${path.module}/currency-demo-deployment.yaml" ) )
     
     wait {
         fields = {
             "status.readyReplicas" = 3
         }
     }
+}
+
+resource "kubernetes_manifest" "demo_service" {
+    manifest = provider::kubernetes::manifest_decode( file( "${path.module}/currency-demo-service.yaml" ) )
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "endpoint" {
-  value = data.aws_eks_cluster.this.endpoint
+data "kubernetes_service" "currency_demo" {
+  metadata {
+    name = kubernetes_manifest.demo_service.manifest.metadata.name
+  }
+}
+
+output "service_hostname" {
+  value = data.kubernetes_service.currency_demo.status[0].load_balancer[0].ingress[0].hostname
 }
